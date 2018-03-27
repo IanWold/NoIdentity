bdfa72ff763685d25ead1512e0525fc663f5cca5
Ian Wold <ilwold@hotmail.com>
2018-03-27 13:42:44 -0500

    Added ugly Manage Account page

---
+++ Source/NoIdentity/Controllers/ManageController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using NoIdentity.Business;
+using NoIdentity.ViewModels;
+
+namespace NoIdentity.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        public IActionResult Account(int id)
+        {
+            try
+            {
+                return View(new ManageAccountViewModel(Business.User.GetById(id)));
+            }
+            catch (ArgumentException)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Account(ManageAccountViewModel model, string redirectUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                //Do things
+            }
+
+            return RedirectToAction(redirectUrl);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Users()
+        {
+            return View(Business.User.GetAllByRole(0).ToList());
+        }
+    }
+}
---
+++ Source/NoIdentity/ViewModels/ManageAccountViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NoIdentity.Business;
+using System.ComponentModel;
+
+namespace NoIdentity.ViewModels
+{
+    public class ManageAccountViewModel
+    {
+        public ManageAccountViewModel() { }
+
+        public ManageAccountViewModel(User user)
+        {
+            Username = user.Username;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+        }
+
+        #region Properties
+
+        public string Username { get; set; }
+
+        [DisplayName("Old Password")]
+        public string OldPassword { get; set; }
+
+        [DisplayName("Verify Old Password")]
+        public string VerifyOldPassword { get; set; }
+
+        [DisplayName("New Password")]
+        public string NewPassword { get; set; }
+
+        [DisplayName("Verify New Password")]
+        public string VerifyNewPassword { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        #endregion
+    }
+}
