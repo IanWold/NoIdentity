20ba913f94bc91664ea0ce8e21f15c9166231ee1
Ian Wold <ilwold@hotmail.com>
2018-03-29 12:49:32 -0500

    Add custom AuthManager class to reduce authentication code

---
+++ Source/NoIdentity.AuthManager/AuthManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NoIdentity.AuthManager
+{
+    public class AuthManager
+    {
+        ClaimsIdentity _identity;
+
+        HttpContext _context;
+
+        public AuthManager(HttpContext context)
+        {
+            _context = context;
+            _identity = new ClaimsIdentity(new Claim[] { }, CookieScheme);
+        }
+
+        public AuthManager(HttpContext context, params (string type, string value)[] claims) : this(context) =>
+            _identity.AddClaims(claims.Select(c => new Claim(c.type, c.value)));
+
+        public async Task SignInAsync(AuthenticationProperties properties) =>
+            await _context.SignInAsync(CookieScheme, new ClaimsPrincipal(_identity), properties);
+
+        public async Task SignOutAsync() =>
+            await _context.SignOutAsync(CookieScheme);
+
+        public static async Task SignOutAsync(HttpContext context) =>
+            await context.SignOutAsync(CookieScheme);
+
+        public static string CookieScheme =>
+            CookieAuthenticationDefaults.AuthenticationScheme;
+    }
+}
--- Source/NoIdentity/Controllers/AuthenticationController.cs
+++ Source/NoIdentity/Controllers/AuthenticationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NoIdentity.Business;
 using NoIdentity.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -11,6 +13,9 @@ namespace NoIdentity.Controllers
 {
     public class AuthenticationController : Controller
     {
+        // For .NET Framework - can encapsulate long call in function:
+        // static  AuthManager() => Request.GetOwinContext().Authentication;
+
         public IActionResult Login()
         {
             return View();
@@ -35,57 +40,74 @@ namespace NoIdentity.Controllers
         {
             try
             {
-                if (ModelState.IsValid)
+                // Validate model state
+                // Get user matching provided login credentials
+                // Get role for user
+                if (ModelState.IsValid
+                    && Business.User.GetByUsername(model.Username) is User user
+                    && user.Password == model.Password
+                    && Role.GetById(user.RoleId) is Role role
+                    && role.Id > -1)
                 {
-                    // Get user matching provided login credentials
-                    if (Business.User.GetByUsername(model.Username) is Business.User user && user.Password == model.Password)
+                    // Construct a list of claims to log the user in with. This data is stored in session
+                    var claims = new List<Claim>()
                     {
-                        // Construct a list of claims to log the user in with. This data is stored in session
-                        var claims = new List<Claim>()
-                        {
-                            new Claim(ClaimTypes.Name, user.FullName),
-                            new Claim(ClaimTypes.NameIdentifier, user.Username),
-                            new Claim("Id", user.Id.ToString()),
-                            new Claim("LastModifiedDate", user.LastModifiedDate.ToString())
-                        };
+                        new Claim(ClaimTypes.Name, user.FullName),
+                        new Claim(ClaimTypes.NameIdentifier, user.Username),
+                        new Claim("Id", user.Id.ToString()),
+                        new Claim("LastModifiedDate", user.LastModifiedDate.ToString())
+                    };
 
-                        // Need to create a ClaimsIdentity specifying the cookie schema (from Startup.cs)
-                        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    // Need to create a ClaimsIdentity specifying the cookie schema (from Startup.cs)
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                        // Demonstrates how to use roles with this approach
-                        var role = Business.Role.GetById(user.RoleId);
-                        if (role.Id > -1)
+                    await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(identity),
+                        new AuthenticationProperties()
                         {
-                            identity.AddClaim(new Claim(ClaimTypes.Role, role.Name));
+                            IsPersistent = false
                         }
-                        
-                        await HttpContext.SignInAsync(
-                            CookieAuthenticationDefaults.AuthenticationScheme,
-                            new ClaimsPrincipal(identity),
-                            new AuthenticationProperties()
-                            {
-                                IsPersistent = false
-                            }
-                        );
+                    );
 
-                        /* If you are using .NET Framework, use the following alternate code to sign in:
-                         * 
-                         * var context = Request.GetOwinContext();
-                         * var authManager = context.Authentication;
-                         *
-                         * authManager.SignIn(
-                         *     new AuthenticationProperties()
-                         *     {
-                         *         IsPersistent = false
-                         *     },
-                         *     identity
-                         * );
-                         */
+                    /* If you want to use my custom AuthManager class to manage all of the above:
+                     * 
+                     * await new AuthManager.AuthManager(HttpContext, new[] {
+                     *     (ClaimTypes.Name, user.FullName),
+                     *     (ClaimTypes.NameIdentifier, user.Username),
+                     *     ("Id", user.Id.ToString()),
+                     *     ("LastModifiedDate", user.LastModifiedDate.ToString()),
+                     *     (ClaimTypes.Role, role.Name)
+                     * }).SignInAsync(new AuthenticationProperties() {
+                     *     IsPersistent = false
+                     * });
+                     */
 
-                        // This can be specified when you register cookie authentication in Startup.cs
-                        return RedirectToAction("Index", "Home");
-                    }
-                    else throw new ArgumentException("Username or Password incorrect.");
+                    /* If you are using .NET Framework, use the following alternate code to sign in:
+                        * 
+                        * var context = Request.GetOwinContext();
+                        * var authManager = context.Authentication;
+                        *
+                        * authManager.SignIn(
+                        *     new AuthenticationProperties()
+                        *     {
+                        *         IsPersistent = false
+                        *     },
+                        *     identity
+                        * );
+                        * 
+                        * // No problem with the following, I figure (see static method at top of file):
+                        * // AuthManager().SignIn(
+                        * //     new AuthenticationProperties()
+                        * //     {
+                        * //         IsPersistent = false
+                        * //     },
+                        * //     identity
+                        * // );
+                        */
+
+                    // This can be specified when you register cookie authentication in Startup.cs
+                    return RedirectToAction("Index", "Home");
                 }
                 else return RedirectToAction("Login", "Authentication");
 
@@ -102,6 +124,8 @@ namespace NoIdentity.Controllers
         /// <returns></returns>
         public async Task<IActionResult> LogOut()
         {
+            // If using the AuthManager project :
+            // await HttpContext.SignOutAsync(AuthManager.AuthManager.CookieScheme);
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
             /* If you are using .NET Framework, use the following alternate code to sign out:
@@ -110,6 +134,9 @@ namespace NoIdentity.Controllers
              * var authManager = context.Authentication;
              *
              * authManager.SignOut("ApplicationCookie");
+             * 
+             * // No problem with the following, I figure (see static method at top of file):
+             * // AuthManager().SignOut("ApplicationCookie");
              */
 
             // This can also be specified when you register cookie authentication in Startup.cs
